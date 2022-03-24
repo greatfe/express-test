38c7bca52b9214912b69dfcf5f9abfaf65c4b0d5
greatfe <blueyear77@gmail.com>
2022-03-25 06:38:37 +0900

    optimize folder struct

--- app/app.js
+++ app/app.js
@@ -2,11 +2,11 @@
 
 
 const express = require("express");
-const home = require("./routes/home")
+const home = require("./src/routes/home")
 
 const app = express();
 app.use("/", home);
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 module.exports = app;
